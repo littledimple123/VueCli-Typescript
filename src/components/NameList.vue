c875e9bbdef76945b251e7b870d7a43451dae695
littledimple123 <cztian1314@163.com>
2021-03-04 08:34:17 +0800

    vue&ts

---
+++ src/components/NameList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span>姓名：{{ userName }}</span
+    >， <span>年龄 ： {{ age }}</span
+    >，
+    <span>性别 ： {{ sex }}</span>
+  </div>
+</template>
+<script lang ='ts'>
+import { Component, Prop, Vue } from "vue-property-decorator";
+@Component
+export default class Namelist extends Vue {
+  //计算userName
+  get userName() {
+    return this.name;
+  }
+  /*
+    @Prop的写法
+      @Prop(Number)
+      @Prop([Number|String])
+      @Prop({type: Number})
+      @Prop({type: Number, default: 1})
+  */
+  @Prop(Number)
+  id!: number;
+  @Prop(String)
+  name!: string;
+  @Prop(Number)
+  age!: number;
+  @Prop({ type: Number, default: 1 })
+  sex!: number;
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="about">
+    <p>{{ firstName }}</p>
+    <p>{{ lastName }}</p>
+    <p>{{ fullName }}</p>
+    <button @click="resetName">修改fullName</button>
+    <hr />
+    <!-- 使用v-bind向子组件传值 -->
+    <nameList v-for="item in arrlist" :key="item.id" v-bind="item"/>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Watch, Vue } from "vue-property-decorator";
+import NameList from "../components/NameList.vue";
+//定义一个接口
+interface Iuser {
+  id: number;
+  name: string;
+  age: number;
+  sex?: number;
+}
+@Component({
+  name: "About", // 相当于原来的export default {name:'About'}
+  components: {
+    // 公共组件写在这里面
+    NameList,
+  },
+})
+export default class Abouts extends Vue {
+  firstName: string = "张";
+  lastName: string = "三";
+  //定义一个数组
+  arrlist: Array<Iuser> = [
+    {
+      id: 0,
+      name: "张三",
+      age: 20,
+      sex: 1,
+    },
+    {
+      id: 1,
+      name: "李三",
+      age: 20,
+      sex: 0,
+    },
+    {
+      id: 2,
+      name: "李三",
+      age: 20
+    },
+  ];
+  // 相当于原来的计算 computed 中的get 和 set
+  get fullName() {
+    return this.firstName + "" + this.lastName;
+  }
+  set fullName(val) {
+    const arrs = val.split("");
+    this.firstName = arrs[0];
+    this.lastName = arrs[1];
+  }
+  // 原来的methods
+  resetName() {
+    this.fullName = "李四";
+  }
+  // 监听
+  @Watch("fullName") // 监听某个属性的变化
+  onfullName(newv: string, oldv: string) {
+    // 自定义监听的方法
+    console.log("newv", newv);
+    console.log("oldv", oldv);
+  }
+  //生命周期函数和之前一样
+  // created() {
+
+  // }
+  // mounted() {
+
+  // }
+}
+</script>
