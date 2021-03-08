4624655c5bf98fab460688bc36f303a854e0b064
littledimple123 <cztian1314@163.com>
2021-03-08 11:38:46 +0800

    lodash.js

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,7 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+import * as lodash from 'lodash';
+declare global { // 全局变量设置
+  const _: typeof lodash
+}
--- src/views/PageThress.vue
+++ src/views/PageThress.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-      pageThree
+    pageThree
+    <br />
+    <p>{{ comItem.join(",") }}</p>
   </div>
 </template>
+<script lang='ts'>
+import { Component, Watch, Vue } from "vue-property-decorator";
+import * as _ from "lodash";
+@Component({
+  name: "PageThress",
+})
+export default class PageThress extends Vue {
+  itemArr: Array<number> = [1, 2, 3, 4, 5, 6];
+  get comItem() {
+    const aab: number = 2;
+    return _.concat(this.itemArr, aab);
+  }
+}
+</script>
